62e61277cfdd5ab21b8cbe7af359f875abe0a688
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-08-17 01:16:57 -0400

    feat: upload contact to DB from frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,46 +12,50 @@ const collectionName = 'dev_collection';
 const port = 8080;
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 logger.info("=================== LOG BREAK ===================")
 logger.info("Starting server...")
 
 // Connect to MongoDB Atlas
 MongoClient.connect(mongoUri)
-  .then(client => {
-    console.log('Connected to MongoDB Atlas');
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    .then(client => {
+        console.log('Connected to MongoDB Atlas');
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
 
-    // Insert example data
-    app.post('/insert', (req, res) => {
-      const data = req.body;
-      collection.insertOne(data)
-        .then(result => {
-          res.status(201).json(result);
-        })
-        .catch(error => {
-          res.status(500).json({ error: `Error inserting data` });
+        // Insert example data
+        app.post('/insert', (req, res) => {
+            const data = req.body;
+            collection.insertOne(data)
+                .then(result => {
+                    logger.info(`Inserted data: ${JSON.stringify(data)}`)
+                    res.status(201).json(result);
+                })
+                .catch(error => {
+                    logger.error(`Error inserting data, error: ${error}`)
+                    logger.error(`Error inserting data, data: ${JSON.stringify(data)}`)
+                    res.status(500).json({ error: `Error inserting data` });
+                });
         });
-    });
 
-    // Retrieve example data
-    app.get('/data', (req, res) => {
-      collection.find({}).toArray()
-        .then(data => {
-          res.status(200).json(data);
-        })
-        .catch(error => {
-          res.status(500).json({ error: 'Error retrieving data' });
+        // Retrieve example data
+        app.get('/data', (req, res) => {
+            collection.find({}).toArray()
+                .then(data => {
+                    res.status(200).json(data);
+                })
+                .catch(error => {
+                    res.status(500).json({ error: 'Error retrieving data' });
+                });
         });
-    });
 
-    // Start the server
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-      logger.info(`Server is running on http://localhost:${port}`)
+        // Start the server
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+            logger.info(`Server is running on http://localhost:${port}`)
+        });
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB Atlas:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error connecting to MongoDB Atlas:', error);
-  });
--- frontend/src/components/contact/ContactView.tsx
+++ frontend/src/components/contact/ContactView.tsx
@@ -13,25 +13,60 @@ function ContactView() {
         setFullNameValue(contact.alias[0])
         setEmailValue(contact.personalEmail || "No email")
         setMiscJsonValue(contact)
-        console.log(contact)        
+        console.log(contact)
     }
 
-    const dummyJson = {test: "test", number: 6, array: [1, 2, 3], object: {a: "a", b: "b"}, float: 3.14, bool: true}
+    const dummyJson = { test: "test", number: 6, array: [1, 2, 3], object: { a: "a", b: "b" }, float: 3.14, bool: true }
 
     const [fullNameValue, setFullNameValue] = useState("");
+    const [notesValue, setNotesValue] = useState("");
+    const [phoneNumberValue, setPhoneNumberValue] = useState("");
+    const [relationshipsValue, setRelationshipsValue] = useState("");
+    const [dateAddedValue, setDateAddedValue] = useState("");
+    const [dateLastUpdatedValue, setDateLastUpdatedValue] = useState("");
+    const [dateLastInteractedValue, setDateLastInteractedValue] = useState("");
     const [emailValue, setEmailValue] = useState("");
     const [miscJsonValue, setMiscJsonValue] = useState<object>(dummyJson);
 
+    const test = {_id: 1000, test: "test from react 2"}
 
+
+    function saveContact() {
+        const date = new Date()
+        const contact = {
+            alias: [fullNameValue],
+            personalEmail: emailValue,
+            notes: notesValue,
+            phoneNumber: phoneNumberValue,
+            relationships: [relationshipsValue],
+            dateAdded: date,
+            dateLastUpdated: date,
+            dateLastInteracted: date
+        }
+        fetch('http://localhost:8080/insert', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(contact)
+        })
+    }
 
     return (
-        
+
         <div>
             <h1>Contact View</h1>
             <button onClick={() => loadRandomContact()}>Load Random Contact</button>
-            <ContactInputBox label={"Name"} textValue={fullNameValue} setValue={setFullNameValue}/>
-            <ContactInputBox label={"Email"} textValue={emailValue} setValue={setEmailValue}/>
-            <ReactJson src={miscJsonValue} name={false} displayDataTypes={false} collapsed={1}/>
+            <button onClick={() => saveContact()}>Save Contact</button>
+            <ContactInputBox label={"Name"} textValue={fullNameValue} setValue={setFullNameValue} />
+            <ContactInputBox label={"Notes"} textValue={notesValue} setValue={setNotesValue} />
+            <ContactInputBox label={"Phone Number"} textValue={phoneNumberValue} setValue={setPhoneNumberValue} />
+            <ContactInputBox label={"Relationships"} textValue={relationshipsValue} setValue={setRelationshipsValue} />
+            <ContactInputBox label={"Email"} textValue={emailValue} setValue={setEmailValue} />
+            <ContactInputBox label={"Date Added"} textValue={dateAddedValue} setValue={setDateAddedValue} />
+            <ContactInputBox label={"Date Last Updated"} textValue={dateLastUpdatedValue} setValue={setDateLastUpdatedValue} />
+            <ContactInputBox label={"Date Last Interacted"} textValue={dateLastInteractedValue} setValue={setDateLastInteractedValue} />
+            <ReactJson src={miscJsonValue} name={false} displayDataTypes={false} collapsed={1} />
         </div>
     );
 }
