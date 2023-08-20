af1dfe648bd325d390abc5221383013d80125ba3
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-08-20 01:57:57 -0400

    hosting: better endoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,45 +16,64 @@ app.use(express.json());
 logger.info("=================== LOG BREAK ===================")
 logger.info("Starting server...")
 
-// Connect to MongoDB Atlas
-MongoClient.connect(mongoUri)
-    .then(client => {
-        console.log('Connected to MongoDB Atlas');
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
+const client = new MongoClient(mongoUri);
 
-        // Insert example data
-        app.post('/insert', (req, res) => {
-            const data = req.body;
-            collection.insertOne(data)
-                .then(result => {
-                    logger.info(`Inserted data: ${JSON.stringify(data)}`)
-                    res.status(201).json(result);
-                })
-                .catch(error => {
-                    logger.error(`Error inserting data, error: ${error}`)
-                    logger.error(`Error inserting data, data: ${JSON.stringify(data)}`)
-                    res.status(500).json({ error: `Error inserting data` });
-                });
-        });
+async function insertData(collection: any, data: any) {
+    try {
+        const result = await collection.insertOne(data);
+        logger.info(`Inserted data: ${JSON.stringify(data)}`);
+        return result;
+    } catch (error) {
+        logger.error(`Error inserting data, error: ${error}`);
+        logger.error(`Error inserting data, data: ${JSON.stringify(data)}`);
+        throw new Error('Error inserting data');
+    }
+}
 
-        // Retrieve example data
-        app.get('/data', (req, res) => {
-            collection.find({}).toArray()
-                .then(data => {
-                    res.status(200).json(data);
-                })
-                .catch(error => {
-                    res.status(500).json({ error: 'Error retrieving data' });
-                });
-        });
+async function retrieveData(collection: any) {
+    try {
+        const data = await collection.find({}).toArray();
+        return data;
+    } catch (error) {
+        throw new Error('Error retrieving data');
+    }
+}
 
-        // Start the server
-        app.listen(port, () => {
-            console.log(`Server is running on http://localhost:${port}`);
-            logger.info(`Server is running on http://localhost:${port}`)
+
+
+
+// Insert example data
+app.post('/insert', (req, res) => {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const data = req.body;
+    collection.insertOne(data)
+        .then(result => {
+            logger.info(`Inserted data: ${JSON.stringify(data)}`)
+            res.status(201).json(result);
+        })
+        .catch(error => {
+            logger.error(`Error inserting data, error: ${error}`)
+            logger.error(`Error inserting data, data: ${JSON.stringify(data)}`)
+            res.status(500).json({ error: `Error inserting data` });
         });
-    })
-    .catch(error => {
-        console.error('Error connecting to MongoDB Atlas:', error);
-    });
+});
+
+// Retrieve example data
+app.get('/data', (req, res) => {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    collection.find({}).toArray()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'Error retrieving data' });
+        });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    logger.info(`Server is running on http://localhost:${port}`)
+});
