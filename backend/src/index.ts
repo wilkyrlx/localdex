25bcb3ec6ae891d5f40afdfb82ac167af1ae019b
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-08-24 18:41:07 -0400

    refactor: mongo code now in mongoClient class

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,77 +3,59 @@ import { MongoClient } from 'mongodb';
 import cors from 'cors';
 import logger from './util/logger';
 import dotenv from 'dotenv';
+import DatabaseManager from './mongoClient';
 
 dotenv.config();
 logger.info("=================== LOG BREAK ===================")
 logger.info("Starting server...")
-
-// TODO: change admin password
-const mongoUri: string = `mongodb+srv://dbAdmin:${process.env.MONGO_DB_ADMIN_PASSWORD}@localdex-dev-cluster-0.3gefrrj.mongodb.net/?retryWrites=true&w=majority`;
-const dbName: string = 'LocalDex-Dev-Cluster-0';
-const collectionName: string = process.env.MONGO_DB_COLLECTION_NAME || "dev_collection";
 
 const port = 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new MongoClient(mongoUri);
 
-async function insertData(collection: any, data: any) {
-    try {
-        const result = await collection.insertOne(data);
-        logger.info(`Inserted data: ${JSON.stringify(data)}`);
-        return result;
-    } catch (error) {
-        logger.error(`Error inserting data, error: ${error}`);
-        logger.error(`Error inserting data, data: ${JSON.stringify(data)}`);
-        throw new Error('Error inserting data');
-    }
-}
 
-async function retrieveData(collection: any) {
-    try {
-        const data = await collection.find({}).toArray();
-        return data;
-    } catch (error) {
-        throw new Error('Error retrieving data');
-    }
-}
+// function insertData(req: any, res: any) {
+//     const db = client.db(dbName);
+//     const collection = db.collection(collectionName);
+//     const data = req.body;
+//     collection.insertOne(data)
+//         .then(result => {
+//             logger.info(`Inserted data: ${JSON.stringify(data)}`);
+//             res.status(201).json(result);
+//         })
+//         .catch(error => {
+//             logger.error(`Error inserting data, error: ${error}`);
+//             logger.error(`Error inserting data, data: ${JSON.stringify(data)}`);
+//             res.status(500).json({ error: `Error inserting data` });
+//         });
+// }
+
+// function getData(req: any, res: any) {
+//     const db = client.db(dbName);
+//     const collection = db.collection(collectionName);
+//     collection.find({}).toArray()
+//         .then(data => {
+//             res.status(200).json(data);
+//         })
+//         .catch(error => {
+//             res.status(500).json({ error: `Error retrieving data: ${error}` });
+//         });
+// }
+
+
+const databaseManager = new DatabaseManager();
 
 
 async function main() {
-    await client.connect();
+    await databaseManager.databaseConnect()
 
     // Insert example data
-    app.post('/insert', (req, res) => {
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-        const data = req.body;
-        collection.insertOne(data)
-            .then(result => {
-                logger.info(`Inserted data: ${JSON.stringify(data)}`)
-                res.status(201).json(result);
-            })
-            .catch(error => {
-                logger.error(`Error inserting data, error: ${error}`)
-                logger.error(`Error inserting data, data: ${JSON.stringify(data)}`)
-                res.status(500).json({ error: `Error inserting data` });
-            });
-    });
+    app.post('/insert', (res, req) => { databaseManager.insertData(res, req) });
 
     // Retrieve example data
-    app.get('/data', (req, res) => {
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-        collection.find({}).toArray()
-            .then(data => {
-                res.status(200).json(data);
-            })
-            .catch(error => {
-                res.status(500).json({ error: `Error retrieving data: ${error}` });
-            });
-    });
+    app.get('/data', (res, req) => { databaseManager.getData(res, req) });
 
     app.get('/helloWorld', (req, res) => {
         res.status(200).json({ message: 'Hello World!' });
---
+++ backend/src/mongoClient.ts
@@ -0,0 +1,53 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import logger from './util/logger';
+
+dotenv.config();
+
+// TODO: change admin password, move into class
+const mongoUri: string = `mongodb+srv://dbAdmin:${process.env.MONGO_DB_ADMIN_PASSWORD}@localdex-dev-cluster-0.3gefrrj.mongodb.net/?retryWrites=true&w=majority`;
+const dbName: string = 'LocalDex-Dev-Cluster-0';
+const collectionName: string = process.env.MONGO_DB_COLLECTION_NAME || "dev_collection";
+
+class DatabaseManager {
+    private client: MongoClient;
+
+    constructor() {
+        this.client = new MongoClient(mongoUri);
+    }
+
+    private getCollection() {
+        const db = this.client.db(dbName);
+        return db.collection(collectionName);
+    }
+
+    async databaseConnect() {
+        return this.client.connect();
+    }
+
+    async insertData(req: any, res: any) {
+        const collection = this.getCollection();
+        const data = req.body;
+        try {
+            const result = await collection.insertOne(data);
+            logger.info(`Inserted data: ${JSON.stringify(data)}`);
+            res.status(201).json(result);
+        } catch (error) {
+            logger.error(`Error inserting data, error: ${error}`);
+            logger.error(`Error inserting data, data: ${JSON.stringify(data)}`);
+            res.status(500).json({ error: 'Error inserting data' });
+        }
+    }
+
+    async getData(req: any, res: any) {
+        const collection = this.getCollection();
+        try {
+            const data = await collection.find({}).toArray();
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json({ error: `Error retrieving data: ${error}` });
+        }
+    }
+}
+
+export default DatabaseManager;
