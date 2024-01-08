8292e48784e5e5e59d6d1637d1fa89b373da6198
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2024-01-07 19:07:46 -0500

    frontend: basic map feature with mapboxgl

---
+++ frontend/src/components/map/BasicMapView.tsx
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Map, { ViewStateChangeEvent, MapLayerMouseEvent, Source, Layer, PointLike, MapRef } from "react-map-gl"
+
+function BasicMapView() {
+
+    const [viewState, setViewState] = useState({
+        longitude: -71.4040,
+        latitude: 41.8278,
+        zoom: 12,
+        bearing: 0,
+        pitch: 0,
+        padding: { top: 1, bottom: 20, left: 1, right: 1 }
+    })
+
+    //useRef hook for a map reference
+    const mapRef = useRef<MapRef>(null)
+
+    const mapboxAccessToken: string = process.env.REACT_APP_MAPBOXGL_API_KEY as string || "NONE_MAPBOX_TOKEN"
+
+    return (
+        <div className="map-demo">
+            <div className="map-demo-map">
+                <Map
+                    mapboxAccessToken={mapboxAccessToken}
+                    latitude={viewState.latitude}
+                    longitude={viewState.longitude}
+                    zoom={viewState.zoom}
+                    pitch={viewState.pitch}
+                    bearing={viewState.bearing}
+                    padding={viewState.padding}
+                    onMove={(ev: ViewStateChangeEvent) => setViewState(ev.viewState)}
+                    // onClick={(e: MapLayerMouseEvent) => onMouseClick(e)}
+                    // FIXME: This is too big, and the 0.9 factor is pretty hacky
+                    style={{ width: window.innerWidth * 0.7, height: window.innerHeight * 0.7 }}
+                    mapStyle={'mapbox://styles/mapbox/light-v10'}
+                    ref={mapRef}>
+
+                    {/* <Source id="geo_data" type="geojson" data={overlay}>
+                        <Layer {...geoLayer} />
+                    </Source> */}
+                </Map>
+            </div>
+
+        </div>
+    )
+}
+
+export default BasicMapView
--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,8 +1,11 @@
+import BasicMapView from "../components/map/BasicMapView";
+
 function MapPage() {
     return (
         <div>
             <h1>Map</h1>
             <p>This is the map page</p>
+            <BasicMapView />
         </div>
     );
 }
